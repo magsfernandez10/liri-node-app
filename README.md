f7240d9b415d750ed0ebd173fe522786a4cb1213
Magaly Fernandez <magalyfernandez@Magalys-MacBook-Pro.local>
2018-11-07 16:42:41 -0600

    updates

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # liri-node-app
+
+1. Make it so liri.js can take in one of the following commands:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+
+  A. `Search the Bands in Town Artist Events API by typing node liri.js concert-this artist name. It should display:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+B.  Search the Spotify API by typing node liri.js spotify-this-song. It should display: node liri.js spotify-this-song song name
+
+   * This will show the following information about the song in your terminal/bash window
+
+     * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+   * If no song is provided then your program will default to "Not Working"
+
+  
+
+
+C. `Search the OMDBI API by typing node liri.js movie-this. It should display: node liri.js movie-this name of movie
+
+   * This will output the following information to your terminal/bash window:
+
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+   D. liri.js do-what-it-says` command. It should display: node liri.js do what-it-says any value
+
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+     video of instructions can be found here:
+
+     https://drive.google.com/open?id=1uqvcqgMqsanwu1urax-Vpij74UjfosE2
+
+     
+
+    
--- liri.js
+++ liri.js
@@ -1,30 +1,106 @@
 require("dotenv").config();
 
-// Here we include the weather-js so we can use it in our Node application.
-var keys = require("./keys.js")
+
+var keys = require("./keys.js");
+var request = require('request');
+var moment = require('moment');
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+var fs = require('fs');
+var input = process.argv;
+var command  = input[2];
+var searchThing = input[3];
 
-var request = require("request");
+switch (command ) {
+	case "concert-this":
+        concert(searchThing);
+        break;
 
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-   // console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
+	case "spotify-this-song":
+        spotify(searchThing);
+        break;
 
+	case "movie-this":
+        movie(searchThing);
+        break;
 
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+	case "do-what-it-says":
+        doitanyway();
+        break;
+    
+    default:
+        console.log("Not working");
+};
 
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
+function concert (searchThing){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + searchThing + "/events?app_id=codingbootcamp";
+    // console.log("print something");
+    request (queryUrl, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			let concertThis = JSON.parse(body)[0];
+		  console.log("----------------------------------");
+		  console.log(`Artist: ${searchThing}\n Venue: ${concertThis.venue.name}\n Location: ${concertThis.venue.city} ${concertThis.venue.country}`);
+		  console.log("Time"+ ": " + moment(body.datetime, 'YYYY-MM-DDh-m-s').format('MM/DD/YYYY'));
+		  console.log("----------------------------------");
+		} else {
+		  console.log(error);
+		}
+	  });
+};
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
+// node liri.js spotify-this-song '<song name here>'
+function spotify(searchThing) {
+
+	var spotify = new Spotify(keys.spotify);
+		if (!searchThing){
+        	searchThing = 'Stronger';
+    	}
+		spotify.search({ type: 'track', query: searchThing }, function(err, data) {
+			if (err){
+	            console.log('STOP: Error occurred: ' + err);
+	            return;
+	        }
+			var songInfo = data.tracks.items;
+			console.log("----------------------------------")
+	        console.log(` Artist(s): ${songInfo[0].artists[0].name}\n Song Name: ${songInfo[0].name}\n Preview Link: ${songInfo[0].preview_url}\n Album: ${songInfo[0].album.name}`);
+			console.log("----------------------------------")
+	});
+}
+
+// node liri.js movie-this '<movie name here>'
+function movie(searchThing) {
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + searchThing + "&y=&plot=short&apikey=ba8cb97";
+
+	request(queryUrl, function(error, response, body) {
+		if (!searchThing){
+        	searchThing = 'Pretty Woman';
+    	}
+		if (!error && response.statusCode === 200) {
+			let movieInfo = JSON.parse(body);
+			console.log('----------------------------------');
+			console.log(` Title: ${movieInfo.Title}\n Release Year: ${movieInfo.Year}\n IMDB Rating:${movieInfo.imdbRating}\n Rotten Tomatoes Rating: ${movieInfo.Ratings[1].Value}\n Country: ${movieInfo.Country}\n Language: ${movieInfo.Language}\n Plot: ${movieInfo.Plot}\n Actors: ${movieInfo.Actors}`);
+			console.log('-----------------------------------');
+		}
+	});
+};
+// node liri.js do-what-it-says
+function doitanyway() {
+	fs.readFile('random.txt', "utf8", function(error, data){
+		if (error) {
+    		return console.log(error);
+  		}
+		// Then split it by commas 
+		var dataArr = data.split(",");
+
+		if (dataArr[0] === "spotify-this-song") {
+			var songcheck = dataArr[1].slice(1, -1);
+			spotify(songcheck);
+		} else if (dataArr[0] === "concert-this") {
+			var concertName = dataArr[1].slice(1, -1);
+			concert(concertName);
+		} else if(dataArr[0] === "movie-this") {
+			var movie_name = dataArr[1].slice(1, -1);
+			movie(movie_name);
+		} 
+  	});
+};
